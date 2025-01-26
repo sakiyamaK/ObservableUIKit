40d3f79cd527ecd9e28f8e42158e3a94f763def1
sakiyamaK <sakiyama.k@gmail.com>
2025-01-26 11:51:57 +0900

    observation > tracking/trackingOptional

--- Sources/ObservableUIKit/ObservableNSObject.swift
+++ Sources/ObservableUIKit/ObservableNSObject.swift
@@ -9,37 +9,109 @@ import UIKit
 
 public protocol ObservableUIKit: AnyObject, Sendable {}
 public extension ObservableUIKit {
+
+    @available(*, deprecated, message: "observation is deprecated. Use tracking/trackingOptional instead")
     @MainActor
     @discardableResult
     func observation<T>(
-        tracking: @escaping @Sendable @MainActor () -> T,
+        tracking:  @escaping @Sendable @MainActor () -> T,
         onChange: @escaping (@Sendable @MainActor (Self, T) -> Void),
-        shouldStop: @escaping (@Sendable () -> Bool) = { false },
         useInitialValue: Bool = true,
-        mainThread: Bool = true
+        shouldStop: @escaping (@Sendable () -> Bool) = { false }
     ) -> Self {
-                
+
         if useInitialValue {
             onChange(self, tracking())
         }
-        
+
         _ = withObservationTracking(tracking, onChange: {[weak self] in
-            
+
             guard let self else { return }
 
             Task { @MainActor in
                 onChange(self, tracking())
-                
+
                 if shouldStop() {
                     return
                 }
-                
+
                 self.observation(
                     tracking: tracking,
                     onChange: onChange,
-                    shouldStop: shouldStop,
                     useInitialValue: useInitialValue,
-                    mainThread: mainThread
+                    shouldStop: shouldStop
+                )
+            }
+        })
+        return self
+    }
+
+    @MainActor
+    @discardableResult
+    func tracking<T>(
+        useInitialValue: Bool = true,
+        sendOptional: Bool = false,
+        shouldStop: @escaping (@Sendable () -> Bool) = { false },
+        _ apply: @escaping @Sendable @MainActor () -> T?,
+        onChange: @escaping (@Sendable @MainActor (Self, T) -> Void)
+    ) -> Self {
+
+        if useInitialValue, let value = apply() {
+            onChange(self, value)
+        }
+
+        _ = withObservationTracking(apply, onChange: {[weak self] in
+
+            Task { @MainActor in
+                guard let self, let value = apply() else { return }
+
+                onChange(self, value)
+
+                if shouldStop() {
+                    return
+                }
+
+                self.tracking(
+                    useInitialValue: useInitialValue,
+                    shouldStop: shouldStop,
+                    apply,
+                    onChange: onChange
+                )
+            }
+        })
+        
+        return self
+    }
+
+    @MainActor
+    @discardableResult
+    func trackingOptional<T>(
+        useInitialValue: Bool = true,
+        shouldStop: @escaping (@Sendable () -> Bool) = { false },
+        _ apply: @escaping @Sendable @MainActor () -> T?,
+        onChange: @escaping (@Sendable @MainActor (Self, T?) -> Void)
+    ) -> Self {
+
+        if useInitialValue {
+            onChange(self, apply())
+        }
+
+        _ = withObservationTracking(apply, onChange: {[weak self] in
+
+            Task { @MainActor in
+                guard let self else { return }
+
+                onChange(self, apply())
+
+                if shouldStop() {
+                    return
+                }
+
+                self.tracking(
+                    useInitialValue: useInitialValue,
+                    shouldStop: shouldStop,
+                    apply,
+                    onChange: onChange
                 )
             }
         })

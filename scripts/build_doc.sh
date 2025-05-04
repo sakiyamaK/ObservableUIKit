f29a626a2b5d17a258318288fa77bc84bebb7ab7
sakiyamaK <sakiyama.k@gmail.com>
2025-05-04 14:51:48 +0900

    fix

--- scripts/build_doc.sh
+++ scripts/build_doc.sh
@@ -1,4 +1,8 @@
+rm -rf ./docs
+
 xcrun docc convert ./ObservableUIKit.docc \
   --output-path ./docs \
   --transform-for-static-hosting \
   --hosting-base-path ObservableUIKit
+
+touch ./docs/.nojekyll
